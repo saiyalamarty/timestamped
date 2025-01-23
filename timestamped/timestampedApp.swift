6f2183cd146a63e83ecf582f5bec48e809f20d65
Sai Yalamarty <sai1494@gmail.com>
2025-01-22 23:55:10 -0600

    Initial Commit

---
+++ timestamped/timestampedApp.swift
@@ -0,0 +1,17 @@
+//
+//  timestampedApp.swift
+//  timestamped
+//
+//  Created by Sai Yalamarty on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct timestampedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
